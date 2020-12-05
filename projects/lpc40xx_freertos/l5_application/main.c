6d97b43f0ec79866321b37b46be44f8f6b6f8abd
shivanipradeep.tambatkar <shivanipradeep.tambatkar@sjsu.edu>
2020-12-04 20:46:25 -0800

    Work In progress. Updated maze code. Working on switching displays

--- projects/lpc40xx_freertos/l5_application/main.c
+++ projects/lpc40xx_freertos/l5_application/main.c
@@ -34,124 +34,105 @@ void down_movement(void);
 uint8_t col_count = 0;
 uint8_t row_count = 0;
 
+static void game_start_display(void *params);
+static SemaphoreHandle_t switch_press_indication;
 static void RGB_task(void *params);
-static void RGB_frame(void *params);
+static void RGB_frame(void);
+static void play_the_game(void *params);
+static void switch_game_start_maze_display_task(void *params);
 void read_song(void *p);
 void play_song(void *p);
 void RGB_clear(void *p);
 
 int main(void) {
-  // mp3_init();
+
   movement_counter = xSemaphoreCreateMutex();
 
   gpio_init();
+
+  clear_display();
+  switch_press_indication = xSemaphoreCreateBinary();
+  // static gpio_s change_display_switch_press = {1, 15};
+
+  xTaskCreate(play_the_game, "Play_Game", 4096, NULL, PRIORITY_HIGH, NULL);
+  xTaskCreate(switch_game_start_maze_display_task, "Switch_Press", 1024, NULL,
+              PRIORITY_HIGH, NULL);
+
+  // xTaskCreate(RGB_frame, "RGB_frame", 4096, NULL, PRIORITY_HIGH, NULL);
+  xTaskCreate(game_start_display, "Game_Start", 4096, NULL, PRIORITY_HIGH,
+              NULL);
+  // xTaskCreate(RGB_task, "RGB_task", 2048, NULL, PRIORITY_MEDIUM, NULL);
+
+  /*************************** ACCELEROMETER
+   * ***************************************/
+  // xTaskCreate(action_on_orientation, "Performing_Action", 4096 /
+  // (sizeof(void*)), NULL, PRIORITY_MEDIUM, NULL);
+  /*************************************************************************************/
+
+  /***************** MP3 DECODER***************************/
+  // mp3_init();
   // mp3_mutex = xSemaphoreCreateMutex();
   // mp3_queue = xQueueCreate(1, sizeof(uint8_t[READ_BYTES_FROM_FILE]));
-  clear_display();
-  // xTaskCreate(RGB_frame, "RGB_frame", 4096, NULL, PRIORITY_HIGH, NULL);
-  // xTaskCreate(RGB_task, "RGB_task", 4096, NULL, PRIORITY_HIGH, NULL);
-
-  xTaskCreate(RGB_frame, "RGB_task", 4096, NULL, PRIORITY_HIGH, NULL);
-  // xTaskCreate(RGB_task, "RGB_task", 4096, NULL, PRIORITY_HIGH, NULL);
-
-  // xTaskCreate(read_song, "read_song", (512U * 8) / sizeof(void *), (void
-  // *)NULL, PRIORITY_LOW, NULL);
-  // xTaskCreate(play_song, "play_song", (512U * 4) / sizeof(void
-  // *), (void *)NULL, PRIORITY_HIGH, NULL);
-
-  // xTaskCreate(action_on_orientation, "Performing_Action",4096 / (sizeof(void
-  // *)), NULL, PRIORITY_HIGH, NULL);
-
-  puts("Starting RTOS");
-  vTaskStartScheduler(); // This function never returns unless RTOS
-                         // scheduler runs out of memory and fails
+  // xTaskCreate(read_song, "read_song", (512U * 8) / sizeof(void
+  // *),(void*)NULL, PRIORITY_LOW, NULL); xTaskCreate(play_song, "play_song",
+  // (512U* 4) / sizeof(void*), (void *)NULL, PRIORITY_HIGH, NULL);
+  /***********************************************************************************/
+  vTaskStartScheduler();
 
   return 0;
 }
 
-static void RGB_frame(void *params) {
+static void RGB_frame(void) {
   gpio_init();
   while (1) {
-
     maze_one_frame();
+  }
+  delay__ms(50);
+}
+
+static void game_start_display(void *params) {
+  gpio_init();
+  while (1) {
+    game_start_display_frame();
+  }
+  delay__ms(50);
+}
+
+void play_the_game(void *params) {
+
+  while (true) {
+    if (xSemaphoreTake(switch_press_indication, 1000)) {
+      RGB_frame();
+      vTaskDelay(250);
+    }
   }
 }
 
 void RGB_task(void *params) {
   while (1) {
 
-    // vTaskDelay(10);
-    // // tom_image(row_count, col_count);
-    // vTaskDelay(5);
-    // tom_clear_image(row_count, col_count);
-    // tom_image_1(3, 4);
-    // jerry_image(15, 15);
+    tom_image_2(row_count, col_count);
 
-    // vTaskDelay(10);
-    // delay__ms(15);
-    jerry_image(row_count, col_count);
-    vTaskDelay(25);
-
-    // delay__ms(5);
-    jerry_image_clear(row_count, col_count);
-    delay__ms(25);
-  }
-}
-void read_song(void *p) {
-  const char *filename = "RangDeBasanti.mp3";
-  static uint8_t bytes_to_read[READ_BYTES_FROM_FILE];
-  FRESULT result;
-  FIL file;
-
-  result = f_open(&file, filename, FA_OPEN_EXISTING | FA_READ);
-  UINT bytes_read;
-  while (1) {
-    xSemaphoreTake(mp3_mutex, portMAX_DELAY);
-
-    result =
-        f_read(&file, &bytes_to_read[0], READ_BYTES_FROM_FILE, &bytes_read);
-    if (0 != result) {
-      printf("Result of %s is %i\n", filename, result);
-    }
-
-    xSemaphoreGive(mp3_mutex);
-    xQueueSend(mp3_queue, &bytes_to_read[0], portMAX_DELAY);
+    // jerry_image(row_count, col_count);
   }
 }
 
-void play_song(void *p) {
-  static uint8_t bytes_to_read[READ_BYTES_FROM_FILE];
-  static uint8_t current_count = 0;
-  uint32_t start_index = 0;
-  while (1) {
+static void switch_game_start_maze_display_task(void *params) {
+  gpio_s change_display_switch_press = {1, 15};
 
-    if (current_count == 0) {
-      xQueueReceive(mp3_queue, &bytes_to_read[0], portMAX_DELAY);
-    }
-    start_index = (current_count * MAX_BYTES_TX);
+  while (true) {
+    // Binary Semaphore is set when switch is pressed
+    if (gpio__get(change_display_switch_press)) {
+      xSemaphoreGive(switch_press_indication);
 
-    while (!mp3_dreq_get_status()) {
-#ifdef TEST
-      printf("data not requested\n");
-#endif
-      vTaskDelay(2);
-    }
-    if (xSemaphoreTake(mp3_mutex, portMAX_DELAY)) {
-
-      send_bytes_to_decoder(start_index, &bytes_to_read[0]);
-      xSemaphoreGive(mp3_mutex);
-      if (current_count == (READ_BYTES_FROM_FILE / MAX_BYTES_TX) - 1) {
-        current_count = 0;
-      } else {
-        current_count += 1;
-#ifdef TEST
-        printf("count = %d\n", current_count);
-#endif
-      }
+      vTaskDelay(500);
     }
   }
 }
 
+/*******************************************************************/
+/*                       ACCELEROMETER                             */
+/*******************************************************************/
 void action_on_orientation(void *p) {
   orientation_e value;
 
@@ -215,3 +196,63 @@ void up_movement(void) {
 
   xSemaphoreGive(movement_counter);
 }
+/*******************************************************************/
+
+/*******************************************************************/
+/*                       MP3 DECODER                               */
+/*******************************************************************/
+void read_song(void *p) {
+  const char *filename = "RangDeBasanti.mp3";
+  static uint8_t bytes_to_read[READ_BYTES_FROM_FILE];
+  FRESULT result;
+  FIL file;
+
+  result = f_open(&file, filename, FA_OPEN_EXISTING | FA_READ);
+  UINT bytes_read;
+  while (1) {
+    xSemaphoreTake(mp3_mutex, portMAX_DELAY);
+
+    result =
+        f_read(&file, &bytes_to_read[0], READ_BYTES_FROM_FILE, &bytes_read);
+    if (0 != result) {
+      printf("Result of %s is %i\n", filename, result);
+    }
+
+    xSemaphoreGive(mp3_mutex);
+    xQueueSend(mp3_queue, &bytes_to_read[0], portMAX_DELAY);
+  }
+}
+
+void play_song(void *p) {
+  static uint8_t bytes_to_read[READ_BYTES_FROM_FILE];
+  static uint8_t current_count = 0;
+  uint32_t start_index = 0;
+  while (1) {
+
+    if (current_count == 0) {
+      xQueueReceive(mp3_queue, &bytes_to_read[0], portMAX_DELAY);
+    }
+    start_index = (current_count * MAX_BYTES_TX);
+
+    while (!mp3_dreq_get_status()) {
+#ifdef TEST
+      printf("data not requested\n");
+#endif
+      vTaskDelay(2);
+    }
+    if (xSemaphoreTake(mp3_mutex, portMAX_DELAY)) {
+
+      send_bytes_to_decoder(start_index, &bytes_to_read[0]);
+      xSemaphoreGive(mp3_mutex);
+      if (current_count == (READ_BYTES_FROM_FILE / MAX_BYTES_TX) - 1) {
+        current_count = 0;
+      } else {
+        current_count += 1;
+#ifdef TEST
+        printf("count = %d\n", current_count);
+#endif
+      }
+    }
+  }
+}
+/*******************************************************************/
