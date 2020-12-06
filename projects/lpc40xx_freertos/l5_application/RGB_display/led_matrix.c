f9b9e0ebf7fe5b398a8a2a826ff488081b15d7e2
soumya.sahu <soumya.sahu@sjsu.edu>
2020-12-06 15:05:12 -0800

    design of tom and jerry in comments

--- projects/lpc40xx_freertos/l5_application/RGB_display/led_matrix.c
+++ projects/lpc40xx_freertos/l5_application/RGB_display/led_matrix.c
@@ -242,6 +242,23 @@ void tom_image(uint8_t x, uint8_t y) {
   clear_pixel(x + 3, y + 2);
 }
 
+/*  red 3 x 3 
+    *
+  * * *
+    * 
+*/
+
+
+/*  yellow 3 x 3
+    *
+    * *
+    * 
+*/
+
+
+
+
+
 void tom_move_on_maze(uint8_t x, uint8_t y) {
 
   if (command_up) {
