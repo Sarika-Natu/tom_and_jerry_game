8df2f3586aaf59302b43efc51609a2f0390cafc8
shivanipradeep.tambatkar <shivanipradeep.tambatkar@sjsu.edu>
2020-12-12 01:08:46 -0800

    Updated code with additional states. Modified/Added display screens

--- projects/lpc40xx_freertos/l5_application/RGB_display/display_screen_RGB.h
+++ projects/lpc40xx_freertos/l5_application/RGB_display/display_screen_RGB.h
@@ -1,12 +1,15 @@
 #include "matrix_look_up_table.h"
 
 extern void start_screen_display(void);
-void jerry_image_start_screen(void);
-void tom_image_start_screen(void);
 
 extern void pause_screen_display(void);
-extern void tom_won_display(void);
-extern void jerry_won_display(void);
 
 extern void clear_screen_display(void);
+
 extern void level_display(void);
+
+extern void player_ready_display(void);
+
+extern void player_won_display(void);
+
+extern void game_over_display(void);
